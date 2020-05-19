ed23a1812ae08894c2314802eeb54134993bf8db
Raj Vijay <raj@virtualbeans.com>
2020-05-19 10:03:20 -0400

    Added Context API to manage wizard forms.

--- client/src/pages/Enroll.js
+++ client/src/pages/Enroll.js
@@ -1,15 +1,13 @@
-import React, { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Col, Container, Row, Form } from "react-bootstrap";
 import { EnrollStep1 } from "./../components/EnrollStep1";
 import { EnrollStep2 } from "./../components/EnrollStep2";
+import { EnrollProvider } from "../providers/enrollProvider";
+import moment from "moment";
 
 export const Enroll = () => {
   const [index, setIndex] = useState(() => 1);
-  // const [data, setData] = useReducer(
-  //   // (state, newState) => ({ ...state, ...newState }),// will not work in IE 11.
-  //   (state, newState) => Object.assign(state, newState),
-  //   {}
-  // );
+  const [data, setData] = useState(() => {});
 
   const next = () => {
     if (index === 2) {
@@ -25,6 +23,33 @@ export const Enroll = () => {
     setIndex((index) => index - 1);
   };
 
+  const handleInput = (e, type, ctrlName) => {
+    let value =
+      type === "check"
+        ? e.target.checked
+        : type === "date"
+        ? moment(e).toDate()
+        : e.target.value;
+    let name = type === "date" ? ctrlName : e.target.id;
+    setData((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  };
+
+  useEffect(() => {
+    console.log(data);
+  });
+
+  const enrollData = {
+    onHandleInput: handleInput,
+    onNextClick: next,
+    onPrevClick: prev,
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <div style={{ textAlign: "center" }}>
@@ -33,8 +58,12 @@ export const Enroll = () => {
       <Container className="mt-3">
         <Row xs={10}>
           <Col>
-            {index === 1 && <EnrollStep1 nextClick={next} />}
-            {index === 2 && <EnrollStep2 prevClick={prev} />}
+            <Form onSubmit={handleSubmit}>
+              <EnrollProvider value={enrollData}>
+                {index === 1 && <EnrollStep1 />}
+                {index === 2 && <EnrollStep2 />}
+              </EnrollProvider>
+            </Form>
           </Col>
         </Row>
       </Container>
---
+++ client/src/providers/enrollProvider.js
@@ -0,0 +1,3 @@
+import React from "react";
+export const EnrollContext = React.createContext({});
+export const EnrollProvider = EnrollContext.Provider;
