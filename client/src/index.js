7802ea79c504925a61b2f02edf8cfaa9a53a440b
Chris Were <chris@mozzler.com.au>
2020-05-31 10:39:00 +0930

    Commit basic API endpoint

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/* Import the Verida API and attach to window object -- just for testing */
+import Api from './api/api';
+window.api = Api;
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
